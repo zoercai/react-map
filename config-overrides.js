36eb60d4f0ece3c70abf9a69254de5c074df150d
Ted Chen <ted.chen@eroad.com>
2017-10-03 10:19:28 +1300

    Cleanup

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,10 @@
 const rewireMobX = require('react-app-rewire-mobx');
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireEslint = require('react-app-rewire-eslint');
+const rewirePolyfills = require('react-app-rewire-polyfills');
 const rewireLess = require('./rewire/react-app-rewire-less');
 const rewireSass = require('./rewire/react-app-rewire-sass');
 const rewireCSS = require('./rewire/react-app-rewire-css-scss');
-const rewireEslintrc = require('./rewire/react-app-rewire-eslintrc');
 const rewireAppIndexEntry = require('./rewire/react-app-rewire-app-index-entry');
 const antTheme = require('./ant-theme');
 
@@ -17,6 +18,9 @@ module.exports = function override(config, env) {
     modifyVars: antTheme,
   });
   config = rewireAppIndexEntry(config, env);
-  config = rewireEslintrc(config, env);
+  config = rewireEslint(config, env);
+  if (env !== 'local') {
+    config = rewirePolyfills(config, env);
+  }
   return config;
 };
--- rewire/react-app-rewire-less.js
+++ rewire/react-app-rewire-less.js
@@ -2,26 +2,23 @@ const { getLoader } = require('react-app-rewired');
 const path = require('path');
 
 
-function rewireLess(config, env, options={}) {
+function rewireLess(config, env, options = {}) {
   const lessExtension = /\.less$/;
-  const fileLoader = getLoader(
-    config.module.rules, rule => rule.loader && typeof rule.loader === 'string' && rule.loader.endsWith(`file-loader${path.sep}index.js`)
-  );
+  const fileLoader = getLoader(config.module.rules, rule => rule.loader && typeof rule.loader === 'string' && rule.loader.endsWith(`file-loader${path.sep}index.js`));
   fileLoader.exclude.push(lessExtension);
 
-  config.module.rules.push(
-    {
-      test: /\.less$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'less-loader',
-          options: options,
-        }
-      ]
-    }
-  )
+  const lessLoader = {
+    test: /\.less$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'less-loader',
+        options,
+      },
+    ],
+  };
+  config.module.rules.push(lessLoader);
   return config;
 }
 module.exports = rewireLess;
