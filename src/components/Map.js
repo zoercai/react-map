875798e22b7be45b282f74c36a1618baed2040e0
zoercai <zoercai@gmail.com>
2018-03-22 14:46:40 +1100

    different filtering methods

--- src/components/Map.js
+++ src/components/Map.js
@@ -16,119 +16,163 @@ export default class Map extends React.Component {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v9',
-      center: [151.211980, -33.864248],
-      zoom: 15,
+      center: [174.7633, -36.8485],
+      zoom: 10,
     });
 
     this.map.on('load', () => {
       // Lines Layer
-
       const trips = this.props.store.trips;
-
       this.map.addSource('linesLayer', {
         type: 'geojson',
         data: trips,
       });
 
-      //  trying to filter?
-      trips.features.forEach((feature) => {
-        const vehicleId = feature.properties.vehicleId;
-        const layerId = `linesLayer-${vehicleId}`;
-
-        // Add a layer for this symbol type if it hasn't been added already.
-        if (!this.map.getLayer(layerId)) {
-          this.map.addLayer({
-            id: layerId,
-            type: 'line',
-            source: 'linesLayer',
-            layout: {
-              'line-join': 'round',
-              'line-cap': 'round',
-            },
-            paint: {
-              'line-color': 'rgba(131,7,123,0.6)',
-              'line-width': 5,
-            },
-            filter: ['==', 'vehicleId', vehicleId],
-          });
-          this.layerIds.push(layerId);
-        }
-      });
-
-      // Points layer
-      this.map.addSource('pointsLayer', {
-        type: 'geojson',
-        data: this.props.store.events,
-      });
-
       this.map.addLayer({
-        id: 'pointsLayer',
-        type: 'symbol',
-        source: 'pointsLayer',
+        id: 'trips',
+        type: 'line',
+        source: 'linesLayer',
         layout: {
-          'icon-image': 'rocket-15',
-          'text-field': 'ignition',
-          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-          'text-offset': [0, 0.6],
-          'text-anchor': 'top',
+          'line-join': 'round',
+          'line-cap': 'round',
+        },
+        paint: {
+          'line-color': 'rgba(131,7,123,0.6)',
+          'line-width': 5,
         },
       });
+      //  trying to filter?
+      // trips.features.forEach((feature) => {
+      //   const vehicleId = feature.properties.vehicleId;
+      //   const layerId = `linesLayer-${vehicleId}`;
 
-      this.layerIds.push('pointsLayer');
+      //   // Add a layer for this vehicleId if it hasn't been added already.
+      //   if (!this.map.getLayer(layerId)) {
+      //     this.map.addLayer({
+      //       id: layerId,
+      //       type: 'line',
+      //       source: 'linesLayer',
+      //       layout: {
+      //         'line-join': 'round',
+      //         'line-cap': 'round',
+      //       },
+      //       paint: {
+      //         'line-color': 'rgba(131,7,123,0.6)',
+      //         'line-width': 5,
+      //       },
+      //       filter: ['==', 'vehicleId', vehicleId],
+      //     });
+      //     this.layerIds.push(layerId);
+      //   }
+      // });
+
+      // Points layer
+      // this.map.addSource('pointsLayer', {
+      //   type: 'geojson',
+      //   data: this.props.store.events,
+      // });
+      // this.map.addLayer({
+      //   id: 'pointsLayer',
+      //   type: 'symbol',
+      //   source: 'pointsLayer',
+      //   layout: {
+      //     'icon-image': 'rocket-15',
+      //     'text-field': 'ignition',
+      //     'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+      //     'text-offset': [0, 0.6],
+      //     'text-anchor': 'top',
+      //   },
+      // });
+      // this.layerIds.push('pointsLayer');
     });
   }
 
-  layerIds = [];
+  // layerIds = [];
   linesFiltered = false;
 
   toggleAllTrips = () => {
-    this.layerIds.forEach((layerId) => {
-      if (layerId.indexOf('linesLayer') > -1) {
-        if (this.linesFiltered) {
-          this.map.setLayoutProperty(layerId, 'visibility', 'visible');
-        } else {
-          this.map.setLayoutProperty(layerId, 'visibility', 'none');
-        }
-      }
-    });
+    if (this.linesFiltered) {
+      this.map.setLayoutProperty('trips', 'visibility', 'visible');
+      this.map.setFilter('trips', null);
+    } else {
+      this.map.setLayoutProperty('trips', 'visibility', 'none');
+    }
     this.linesFiltered = !this.linesFiltered;
   };
 
-  toggleLayerType = (layerType) => {
-    this.layerIds.forEach((layerId) => {
-      if (layerId.indexOf(layerType) > -1) {
-        if (this.map.getLayoutProperty(layerId, 'visibility') === 'none') {
-          this.map.setLayoutProperty(layerId, 'visibility', 'visible');
-        } else {
-          this.map.setLayoutProperty(layerId, 'visibility', 'none');
-        }
-      }
-    });
-  };
+  // toggleAllTrips = () => {
+  //   this.layerIds.forEach((layerId) => {
+  //     if (layerId.indexOf('linesLayer') > -1) {
+  //       if (this.linesFiltered) {
+  //         this.map.setLayoutProperty(layerId, 'visibility', 'visible');
+  //       } else {
+  //         this.map.setLayoutProperty(layerId, 'visibility', 'none');
+  //       }
+  //     }
+  //   });
+  //   this.linesFiltered = !this.linesFiltered;
+  // };
+
+  // toggleLayerType = (layerType) => {
+  //   this.layerIds.forEach((layerId) => {
+  //     if (layerId.indexOf(layerType) > -1) {
+  //       if (this.map.getLayoutProperty(layerId, 'visibility') === 'none') {
+  //         this.map.setLayoutProperty(layerId, 'visibility', 'visible');
+  //       } else {
+  //         this.map.setLayoutProperty(layerId, 'visibility', 'none');
+  //       }
+  //     }
+  //   });
+  // };
 
   filterByLayer = (layerPrefix) => {
     this.linesFiltered = true;
-    this.layerIds.forEach((layerId) => {
-      if (layerId.indexOf(layerPrefix) > -1) {
-        this.map.setLayoutProperty(layerId, 'visibility', 'visible');
-      } else {
-        this.map.setLayoutProperty(layerId, 'visibility', 'none');
-      }
-    });
+    this.map.setFilter('trips', ['==', 'vehicleId', layerPrefix]);
+    // this.map.setFilter('trips', ['all', this.map.getFilter('trips'), ['==', 'vehicleId', layerPrefix]]);
   };
+  // filterByLayer = (layerPrefix) => {
+  //   this.linesFiltered = true;
+  //   this.layerIds.forEach((layerId) => {
+  //     if (layerId.indexOf(layerPrefix) > -1) {
+  //       this.map.setLayoutProperty(layerId, 'visibility', 'visible');
+  //     } else {
+  //       this.map.setLayoutProperty(layerId, 'visibility', 'none');
+  //     }
+  //   });
+  // };
 
-  filterByLayerSearch = (event) => {
-    const term = event.target.value.trim().toLowerCase();
+  filterBySensor = (sensorName) => {
     this.linesFiltered = true;
-    this.layerIds.forEach((layerId) => {
-      const layerIdLower = layerId.trim().toLowerCase();
-      if (layerIdLower.indexOf(term) > -1) {
-        this.map.setLayoutProperty(layerId, 'visibility', 'visible');
-      } else {
-        this.map.setLayoutProperty(layerId, 'visibility', 'none');
-      }
-    });
-  };
+    // const features = this.map.queryRenderedFeatures(layerId);
+    // this.map.setFilter('trips', ['all', this.map.getFilter('trips'), ['==', 'sensorName', sensorName]]);
+    this.map.setFilter('trips', ['==', 'sensorName', sensorName]);
+  }
+  // filterBySensor = (sensorName) => {
+  //   this.linesFiltered = true;
+  //   this.layerIds.forEach((layerId) => {
+  //     // const features = this.map.queryRenderedFeatures(layerId);
+  //     // this.map.setFilter(layerId, ['all', this.map.getFilter(layerId), ['==', 'sensorName', sensorName]]);
+  //     this.map.setFilter(layerId, ['==', 'sensorName', sensorName]);
+  //   });
+  // }
+
+  // filterByLayerSearch = (event) => {
+  //   this.linesFiltered = true;
+  //   const term = event.target.value.trim().toLowerCase();
+  //   this.map.setFilter('trips', ['has', 'vehicleId', term]);
+  // };
+  // filterByLayerSearch = (event) => {
+  //   const term = event.target.value.trim().toLowerCase();
+  //   this.linesFiltered = true;
+  //   this.layerIds.forEach((layerId) => {
+  //     const layerIdLower = layerId.trim().toLowerCase();
+  //     if (layerIdLower.indexOf(term) > -1) {
+  //       this.map.setLayoutProperty(layerId, 'visibility', 'visible');
+  //     } else {
+  //       this.map.setLayoutProperty(layerId, 'visibility', 'none');
+  //     }
+  //   });
+  // };
 
   render() {
     return (
@@ -136,9 +180,13 @@ export default class Map extends React.Component {
         <div className={styles.menu}>
           <span className={styles.active} onClick={() => this.toggleAllTrips()}> All Trips </span>
           <span className={styles.active} onClick={() => this.toggleLayerType('pointsLayer')}> Points </span>
-          <span className={styles.active} onClick={() => this.filterByLayer('linesLayer-Eroad-A1')}> Eroad-A1 </span>
-          <span className={styles.active} onClick={() => this.filterByLayer('linesLayer-Eroad-B2')}> Eroad-B2 </span>
-          <span className={styles.active} onClick={() => this.filterByLayer('linesLayer-Eroad-C3')}> Eroad-C3 </span>
+          <span className={styles.active} onClick={() => this.filterBySensor('Input1')}> Input1 </span>
+          <span className={styles.active} onClick={() => this.filterBySensor('Seatbelt')}> Seatbelt </span>
+          <span className={styles.active} onClick={() => this.filterBySensor('Input2')}> Input2 </span>
+          <span className={styles.active} onClick={() => this.filterByLayer('smiley')}> Smiley </span>
+          <span className={styles.active} onClick={() => this.filterByLayer('bigW')}> BigW </span>
+          <span className={styles.active} onClick={() => this.filterByLayer('zigzaghamilton')}> zigzaghamilton </span>
+          <span className={styles.active} onClick={() => this.filterByLayer('longAuckland')}> LongAuckland </span>
           <span className={styles.active} onClick={() => this.filterByLayer('linesLayer-Eroad-D4')}> Eroad-D4 </span>
         </div>{
         }<div
@@ -149,9 +197,9 @@ export default class Map extends React.Component {
           className={styles.map}
         />
 
-        <div className={styles.filterInput}>
+        {/* <div className={styles.filterInput}>
           <input id="filter-input" type="text" name="searchFilter" placeholder="Filter by vehicleId" onChange={this.filterByLayerSearch} />
-        </div>
+        </div> */}
       </div >
     );
   }
